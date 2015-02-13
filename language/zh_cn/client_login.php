8b57b28f50dcd88ddeaff857fd7dafb0e809e1d0
billzhong <billzhong@gmail.com>
2015-02-13 11:30:21 -0330

    client login

---
+++ language/zh_cn/client_login.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Language definitions for the Client Login controller
+ * 
+ * @package blesta
+ * @subpackage blesta.language.zh_cn
+ * @copyright Copyright (c) 2010, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+// Error
+$lang['ClientLogin.!error.unknown_user'] = "用户名未知或密码错误。";
+
+
+// Success
+$lang['ClientLogin.!success.reset_sent'] = "一封确认邮件已经发送。";
+
+
+// Login
+$lang['ClientLogin.index.page_title'] = "登入";
+$lang['ClientLogin.index.description'] = "请先登入再访问会员中心。";
+$lang['ClientLogin.index.login_heading'] = "登入";
+$lang['ClientLogin.index.field_username'] = "用户名";
+$lang['ClientLogin.index.field_password'] = "密码";
+$lang['ClientLogin.index.field_rememberme'] = "在本机记住密码";
+$lang['ClientLogin.index.field_loginsubmit'] = "登入";
+$lang['ClientLogin.index.link_resetpassword'] = "重置密码";
+
+
+// OTP
+$lang['ClientLogin.otp.page_title'] = "登入";
+$lang['ClientLogin.otp.description'] = "请输入一次性密码。";
+$lang['ClientLogin.otp.login_heading'] = "登入";
+$lang['ClientLogin.otp.field_otp'] = "一次性密码";
+$lang['ClientLogin.otp.field_submit'] = "登入";
+
+
+// Reset
+$lang['ClientLogin.reset.page_title'] = "重置密码";
+$lang['ClientLogin.reset.reset_heading'] = "重置密码";
+$lang['ClientLogin.reset.description'] = "请输入用户名来获取密码重置链接。";
+$lang['ClientLogin.reset.field_username'] = "用户名";
+$lang['ClientLogin.reset.field_resetsubmit'] = "重置密码";
+$lang['ClientLogin.reset.link_login'] = "取消重置";
+
+
+// Confirm Reset
+$lang['ClientLogin.confirmreset.page_title'] = "确认密码重置";
+$lang['ClientLogin.confirmreset.reset_heading'] = "确认密码重置";
+$lang['ClientLogin.confirmreset.description'] = "请输入新密码。";
+$lang['ClientLogin.confirmreset.field_new_password'] = "新密码";
+$lang['ClientLogin.confirmreset.field_confirm_password'] = "确认新密码";
+$lang['ClientLogin.confirmreset.field_resetsubmit'] = "保存密码";
+$lang['ClientLogin.confirmreset.link_login'] = "取消重置";
+?>
