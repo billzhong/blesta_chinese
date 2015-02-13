a99fa49f42d7c1228073a2f70fb79e182a6516b6
billzhong <billzhong@gmail.com>
2015-02-13 11:30:09 -0330

    404 and custom

---
+++ language/zh_cn/_404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Language definitions for the _404 controller/views
+ * 
+ * @package blesta
+ * @subpackage blesta.language.zh_cn
+ * @copyright Copyright (c) 2010, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+$lang['404.heading_error'] = "404";
+$lang['404.text_error'] = "对不起，该页不存在或被转移。";
+?>
---
+++ language/zh_cn/_custom.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Custom Language definitions used throughout models
+ * 
+ * @package blesta
+ * @subpackage blesta.language.zh_cn
+ * @copyright Copyright (c) 2010, Phillips Data, Inc.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ */
+
+// Payment Types
+# REQUIRED FORMAT: $lang['_PaymentTypes.payment_type_here'] = "The payment type.";
+$lang['_PaymentTypes.cash'] = "现金";
+$lang['_PaymentTypes.check'] = "支票";
+$lang['_PaymentTypes.money_order'] = "汇票";
+$lang['_PaymentTypes.in_house_credit'] = "代金券";
+
+
+// Contact Types
+# REQUIRED FORMAT: $lang['_ContactTypes.contact_type_name_here'] = "The contact type.";
+
+
+// Custom Fields
+# REQUIRED FORMAT: $lang['_CustomFields.custom_field_name_here'] = "The custom field.";
+?>
